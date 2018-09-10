57947c649555147befddbe05603d7eb5e64f1898
Daniel Aragão <danielfilhoce@gmail.com>
2018-09-09 23:09:50 -0300

    Adicionando reconhecimento do mapa, porém a limpeza da posição dos agentes está com problemas

--- src/algoritmo/Poupador.java
+++ src/algoritmo/Poupador.java
@@ -1,43 +1,155 @@
 package algoritmo;
 
+import java.awt.Point;
+import java.util.Hashtable;
+import java.util.Map.Entry;
+
 public class Poupador extends ProgramaPoupador {
-	
+	private static final int POUPADOR = 100;
+	private static final int LADRAO = 200;
+	static final int POSITION = 2;
+
+	private int[][] map;
+	private Point currentPosition;
+	private int MAP_M = 30;
+	private int MAP_N = 30;
+	private boolean firstIteration = true;
+	private Hashtable<Integer, Point> agentsMap;
+
+	private void undiscoverMap() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				map[i][j] = -5;
+			}
+		}
+	}
+
+	private void fillMap(Point point, int[][] sensor, int offset, int m, int n) {
+		for (int i = point.y - offset, count_i = 0; count_i < m; count_i++, i++) {
+			for (int j = point.x - offset, count_j = 0; count_j < n; count_j++, j++) {
+				int cellValue = sensor[count_i][count_j];
+
+				if (i >= 0 && j >= 0 && i < MAP_M && j < MAP_N && cellValue != -2) {
+					this.map[i][j] = sensor[count_i][count_j];
+
+					if (cellValue >= LADRAO) {
+						agentsMap.put(LADRAO, new Point(j, i));
+					} else if (cellValue >= POUPADOR && cellValue < LADRAO) {
+						agentsMap.put(POUPADOR, new Point(j, i));
+					}
+				}
+			}
+		}
+	}
+
+	private void clearAgentsFromMap() {
+		if (agentsMap != null) {
+			for (Entry<Integer, Point> tuple : agentsMap.entrySet()) {
+				Point point = tuple.getValue();
+
+				this.map[point.y][point.x] = 0;
+			}
+		}
+
+		this.map[currentPosition.y][currentPosition.x] = 0;
+	}
+
 	public int acao() {
-		
-		int [] visao = sensor.getVisaoIdentificacao();
-		
-		Util.printArrayAsMatrix(visao, 5);
-		System.out.println();
-		
+		try {
+
+			if (firstIteration) {
+				map = new int[MAP_M][MAP_N];
+				undiscoverMap();
+				firstIteration = false;
+			} else {
+				clearAgentsFromMap();
+			}
+
+			agentsMap = new Hashtable<Integer, Point>();
+
+			currentPosition = sensor.getPosicao();
+
+			int[] visao = sensor.getVisaoIdentificacao();
+			fillMap(sensor.getPosicao(), Util.getSensorArrayAsMatrix(visao, 5), 2, 5, 5);
+
+			Util.printMatrix(map);
+
+//		int [] olfato = sensor.getAmbienteOlfatoPoupador();
+
+			Util.printSensorArrayAsMatrix(visao, 5);
+			System.out.println();
+//		Util.printMatrix(Util.getSensorArrayAsMatrix(visao,5));
+			System.out.println();
+
+//		Util.printArrayAsMatrix(olfato, 3);
+//		System.out.println();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		return (int) (Math.random() * 5);
 	}
 
 }
 
 class Util {
-	public static void printArrayAsMatrix(int[] array, int m) {
-		int n = (array.length+1)/m;
-		
-		int i_center = (int) Math.ceil(m/2.0) - 1 ;
-		int j_center = (int) Math.ceil(n/2.0) - 1;
-		
+	public static void printMatrix(int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.printf("%5d", array[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void printSensorArrayAsMatrix(int[] array, int m) {
+		int n = (array.length + 1) / m;
+
+		int i_center = (int) Math.ceil(m / 2.0) - 1;
+		int j_center = (int) Math.ceil(n / 2.0) - 1;
+
 		int offset = 0;
-		for(int i = 0; i < m; i++) {
-			
-			for(int j = 0; j < n; j++) {
-				if(i_center == i && j_center == j) {
+		for (int i = 0; i < m; i++) {
+
+			for (int j = 0; j < n; j++) {
+				if (i_center == i && j_center == j) {
 					offset = 1;
 					System.out.printf("     ");
 					continue;
 				}
-				System.out.printf("%5d", array[i*n + j - offset]);//i*n+j);
+
+				System.out.printf("%5d", array[i * n + j - offset]);// i*n+j);
 			}
+
 			System.out.println();
 		}
+	}
+
+	public static int[][] getSensorArrayAsMatrix(int[] array, int m) {
+		int n = (array.length + 1) / m;
+		int[][] matrix = new int[m][n];
+
+		int i_center = (int) Math.ceil(m / 2.0) - 1;
+		int j_center = (int) Math.ceil(n / 2.0) - 1;
+
+		int offset = 0;
+		for (int i = 0; i < m; i++) {
+
+			for (int j = 0; j < n; j++) {
+				if (i_center == i && j_center == j) {
+					offset = 1;
+					matrix[i][j] = Poupador.POSITION;
+					continue;
+				}
+
+				matrix[i][j] = array[i * n + j - offset];
+			}
+		}
+
+		return matrix;
 	}
 }
 
 // 00 01 02 03 04
 // 05 06 07 08 09
 // 10 11 12 13 14
-
